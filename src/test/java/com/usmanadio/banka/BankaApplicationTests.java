251d67616bc427874a7e752321b6efa205d5a1da
hadeoh <usthmandanfodio@gmail.com>
2020-02-24 18:28:13 -0800

    Initial commit

---
+++ src/test/java/com/usmanadio/banka/BankaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usmanadio.banka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
