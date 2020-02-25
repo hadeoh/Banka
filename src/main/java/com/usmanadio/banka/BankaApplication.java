6b0b3eacda37d87e166fb9ceb8af529c09893080
hadeoh <usthmandanfodio@gmail.com>
2020-02-25 15:40:51 -0800

    Implement security with JWT

--- src/main/java/com/usmanadio/banka/BankaApplication.java
+++ src/main/java/com/usmanadio/banka/BankaApplication.java
@@ -1,8 +1,11 @@
 package com.usmanadio.banka;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +13,17 @@ public class BankaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankaApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 
 }
--- src/main/java/com/usmanadio/banka/repositories/UserRepository.java
+++ src/main/java/com/usmanadio/banka/repositories/UserRepository.java
@@ -4,4 +4,6 @@ import com.usmanadio.banka.models.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
+    boolean existsByEmail(String email);
 }
