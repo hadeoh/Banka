c434a44e5c16a1e26e2f80f2f43b4d48ad319b80
hadeoh <usthmandanfodio@gmail.com>
2020-03-13 04:25:03 +0100

    Implement account viewing based on status and type

--- src/main/java/com/usmanadio/banka/repositories/AccountRepository.java
+++ src/main/java/com/usmanadio/banka/repositories/AccountRepository.java
@@ -1,7 +1,11 @@
 package com.usmanadio.banka.repositories;
 
 import com.usmanadio.banka.models.account.Account;
+import com.usmanadio.banka.models.account.AccountStatus;
+import com.usmanadio.banka.models.account.AccountType;
 import com.usmanadio.banka.models.user.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +16,6 @@ public interface AccountRepository extends JpaRepository<Account, UUID> {
     Account findByAccountNumber(String accountNumber);
     Integer deleteByAccountNumber(String accountNumber);
     List<Account> findAllByUser(User user);
+    Slice<Account> findAllByAccountStatus(AccountStatus accountStatus, Pageable pageable);
+    Slice<Account> findAllByAccountType(AccountType accountType, Pageable pageable);
 }
