e8eebf8504834e175032a61dc3a983fd921460ef
hadeoh <usthmandanfodio@gmail.com>
2020-03-17 12:33:27 +0100

    Optimize tests and add code coverage report

--- src/main/java/com/usmanadio/banka/BankaApplication.java
+++ src/main/java/com/usmanadio/banka/BankaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
@@ -27,9 +26,9 @@ public class BankaApplication {
         return new BCryptPasswordEncoder(10);
     }
 
-    @Bean
-    public JavaMailSenderImpl javaMailSender() {
-        return new JavaMailSenderImpl();
-    }
+//    @Bean
+//    public JavaMailSenderImpl javaMailSender() {
+//        return new JavaMailSenderImpl();
+//    }
 
 }
