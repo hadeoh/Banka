855975c2eceaa371f3ed0176b0fe2f4039953108
hadeoh <usthmandanfodio@gmail.com>
2020-03-17 12:36:03 +0100

    Optimize tests and add code coverage report

--- src/main/java/com/usmanadio/banka/BankaApplication.java
+++ src/main/java/com/usmanadio/banka/BankaApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.mail.internet.MimeMessage;
+import java.io.InputStream;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -26,9 +33,49 @@ public class BankaApplication {
         return new BCryptPasswordEncoder(10);
     }
 
-//    @Bean
-//    public JavaMailSenderImpl javaMailSender() {
-//        return new JavaMailSenderImpl();
-//    }
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSender() {
+            @Override
+            public void send(SimpleMailMessage simpleMailMessage) throws MailException {
+
+            }
+
+            @Override
+            public void send(SimpleMailMessage... simpleMailMessages) throws MailException {
+
+            }
+
+            @Override
+            public MimeMessage createMimeMessage() {
+                return null;
+            }
+
+            @Override
+            public MimeMessage createMimeMessage(InputStream inputStream) throws MailException {
+                return null;
+            }
+
+            @Override
+            public void send(MimeMessage mimeMessage) throws MailException {
+
+            }
+
+            @Override
+            public void send(MimeMessage... mimeMessages) throws MailException {
+
+            }
+
+            @Override
+            public void send(MimeMessagePreparator mimeMessagePreparator) throws MailException {
+
+            }
+
+            @Override
+            public void send(MimeMessagePreparator... mimeMessagePreparators) throws MailException {
+
+            }
+        };
+    }
 
 }
